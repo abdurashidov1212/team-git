c18c2e6ecb5b7fd5adc1a3abeaa1f8a0236c1031
xojakbar <abdurashidovxojiakbar098@gmail.com>
2025-03-18 22:26:00 +0500

    is user1 commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Router,Route,Routes } from "react-router-dom"
 import Home from "./pages/home";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
     </Routes>
     </>
   )
