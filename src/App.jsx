ab73d0ad0841c40fa2383732872c4a7d58ac3cd2
xojakbar <abdurashidovxojiakbar098@gmail.com>
2025-03-18 22:13:09 +0500

    user1 commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import { Router,Route,Routes } from "react-router-dom"
+import Home from "./pages/home";
 
 function App() {
   return (
     <>
-
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
     </>
   )
 }
 
-export default App
+export default App;
