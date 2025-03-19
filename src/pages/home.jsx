394099355d385d343199309d18837ae456bb88a3
xojakbar <abdurashidovxojiakbar098@gmail.com>
2025-03-19 15:10:51 +0500

    one

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,25 @@
+import "./home.css"
+import light from "../assets/light.png"
+
 const Home = ()=>{
     return(
         <>
-        <h1>Home page</h1>
+        <div className="home">
+            <div className="home1">
+
+        <h3>MODERN INTERIOR</h3>
+        <h1>Create <br /> Your   <br />
+        Interior Design.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br />
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim <br />
+veniam, quis nostrud exercitation.</p>
+<button>CONTACT</button>
+</div>
+<div className="home2">
+    <img src={light} alt="light" width={550} height={620} />
+</div>
+
+        </div>
         </>
     )
 }
