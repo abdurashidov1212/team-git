8226c5bfca9f0c322cae1c6f146955cb3d28da4c
xojakbar <abdurashidovxojiakbar098@gmail.com>
2025-03-19 16:12:28 +0500

    about style is added

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,32 @@
+import "./about.css"
+
 const About = ()=>{
     return(
         <>
-        <h1>About page</h1>
+      <div className="about">
+        <div className="about1"> 
+            <h3>3000+ Completed Work <br />
+            Which WE have Successfully Done</h3>
+        </div>
+        <div className="about2">
+            <div className="ab1">
+                <h2>980</h2>
+                <p>Project</p>
+            </div>
+            <div className="ab2">
+                <h2>520</h2>
+                <p>Happy Client</p>
+            </div>
+            <div className="ab3">
+                <h2>330</h2>
+                <p>Winners</p>
+            </div>
+            <div className="ab4">
+                <h2>120</h2>
+                <p>Recoment</p>
+            </div>
+        </div>
+      </div>
         </>
     )
 }
