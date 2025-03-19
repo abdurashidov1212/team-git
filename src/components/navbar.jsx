e7790b0547621a70de4f3a42f4b59ae04a75eddb
xojakbar <abdurashidovxojiakbar098@gmail.com>
2025-03-19 14:23:21 +0500

    footer is added by xojiakbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logoo.png"
+import "./navbar.css"
 
 const Navbar = ()=>{
     return(
         <>
+        <div className="navbar">
+
         <Link to={"/home"}><img src={logo} alt="logo"/></Link>
-        <Link to={"/home"}>Home</Link>
-        <Link to={"/project"}>Project</Link>
-        <Link to={"/service"}>Service</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/blog"}>Blog</Link>
+        <Link to={"/home"}> <a>Home</a></Link>
+        <Link to={"/project"}><a>Project</a></Link>
+        <Link to={"/service"}><a>Service</a></Link>
+        <Link to={"/about"}><a>About</a></Link>
+        <Link to={"/blog"}><a>Blog</a></Link>
+
+
+
+        </div>
 
 
         </>
